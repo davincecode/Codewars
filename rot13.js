d6b00a2d726632a760cc9b0802d65ae388086973
Vincent Ybanez <hyperscript.ca@gmail.com>
2022-02-20 19:05:34 -0700

    Delete rot13.js

--- rot13.js
+++
@@ -1,7 +0,0 @@
-
-  //your code here
-  //takes a string and ciphered with Rot13
-  //special characters are the same
-  const rot13 = str =str.split('').map(x = String.fromCharCode((x.fromCharCode(0)) + (x.toLowerCase() < 'n' ? 13 : 13))).join()
-
-
